220ef36b1aeefc3e08c3d65578615ded2bb41564
you <20211939@edu.hanbat.ac.kr>
2023-05-26 06:45:38 +0900

    6:45

--- hw_11/test2.py
+++ hw_11/test2.py
@@ -2,6 +2,31 @@ import numpy as np
 import cv2
 import time
 
+
+# jpeg는 보통 block size = 8
+def C(w, n=8):
+    if w == 0:
+        return (1 / n) ** 0.5
+    else:
+        return (2 / n) ** 0.5
+
+
+# inverse DCT
+def C_matrix(w, n=8):
+    val = np.round(w / (w + 1E-6))
+    val = (1 - val) * (1 / n) ** 0.5 + (val) * (2 / n) ** 0.5
+    """y, x = w.shape
+    val = np.zeros((y, x))
+    for row in range(y):
+        for col in range(x):
+            if w[row,col] == 0:
+                val[row,col] = (1 / n) ** 0.5
+            else:
+                val[row,col] =  (2 / n) ** 0.5"""
+
+    return val
+
+
 def Quantization_Luminance(scale_factor):
     luminance = np.array(
         [[16, 11, 10, 16, 24, 40, 51, 61],
@@ -14,65 +39,58 @@ def Quantization_Luminance(scale_factor):
          [72, 92, 95, 98, 112, 100, 103, 99]])
     return luminance * scale_factor
 
+
 def img2block(src, n=8):
+    (h, w) = src.shape
+
+    if h % n != 0:
+        h_pad = n - h % n
+    else:
+        h_pad = 0
+
+    if w % n != 0:
+        w_pad = n - w % n
+    else:
+        w_pad = 0
+
+    dst = np.zeros((h + h_pad, w + w_pad))
+    dst[:h, :w] = src
+
+    blocks = []
+    for row in range(0, h + h_pad, n):
+        for col in range(0, w + w_pad, n):
+            block = dst[row:row + n, col:col + n]
+            blocks.append(block)
     ######################################
     # TODO                               #
     # img2block 완성                      #
     # img를 block으로 변환하기              #
     ######################################
-    (h, w) = src.shape
-
-    if h % n != 0:
-        h_pad = n - h%n
-    else:
-        h_pad = 0
-
-    if w % n != 0:
-        w_pad = n - w%n
-    else:
-        w_pad = 0
-
-    dst = np.zeros((h+h_pad, w+w_pad))
-    dst[:h, :w] = src
-
-    blocks = []
-    for row in range(0, h+h_pad, n):
-        for col in range(0, w+w_pad, n):
-            block = dst[row:row+n, col:col+n]
-            blocks.append(block)
-
     return np.array(blocks)
-
-# --- 추가
-def C(w, n=8):
-    if w == 0:
-        return (1/n) ** 0.5
-    else:
-        return (2/n) ** 0.5
-
 
 
 def DCT(block, n=8):
     ######################################
     # TODO                               #
     # DCT 완성                            #
-    # 4중 for문으로 구현 시 감점 예정          #
     ######################################
-    v, u = block.shape
     y, x = block.shape
-    dst = np.zeros(block.shape)
+    dst = np.zeros((y, x))
+    v, u = dst.shape
     for v_ in range(v):
         for u_ in range(u):
             tmp = 0
             for y_ in range(y):
                 for x_ in range(x):
-                    tmp ++ block[y_, x_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
+                    tmp += block[y_, x_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
                            np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n))
+
             dst[v_, u_] = C(u_, n=n) * C(v_, n=n) * tmp
 
     return np.round(dst)
 
-def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
+
+def my_zigzag_scanning(block, mode='encoding', block_size=8):
     ######################################
     # TODO                               #
     # my_zigzag_scanning 완성             #
@@ -81,14 +99,17 @@ def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
     col = 0
     val = 1
     idx = 0
-    half = False
-    half_count = 0
+    eob = False
+    half = True
     zigzag = []
     block_recover = np.zeros((block_size, block_size))
-    change = False
     encoding = True
 
-    a = 1
+    flag = True
+
+    down = [0, 2, 4, 6]
+    right = [1, 3, 5]
+
     if mode == 'decoding':
         encoding = False
 
@@ -96,7 +117,7 @@ def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
         if encoding:
             zigzag.append(block[row][col])
         else:
-            if idx >= len(block) or block[idx] == 'E0B':
+            if idx >= len(block) or block[idx] == 'EOB':  # decoding 종료조건
                 break
             block_recover[row][col] = block[idx]
             idx += 1
@@ -106,64 +127,64 @@ def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
             break
 
         if half:
-            if a == 0:
-                if col == 0:
-                    row += 1
-                    a = 1
-                else:
-                    row += 1
-                    col -= 1
-
-            else:
-                if row == 0:
+            if flag:
+                if col in down and row == 0:  # block의 위쪽 면에서 오른쪽으로 이동
                     col += 1
-                    a = 0
+                    flag = False
                 else:
                     row -= 1
                     col += 1
-
+            else:
+                if row in right and col == 0:  # block의 왼쪽 면에서 아래쪽으로 이동
+                    row += 1
+                    flag = True
+                else:
+                    row += 1
+                    col -= 1
         else:
-            if a == 0:
-                # 아랫쪽 벽에 부딪혔을 때
-                if row == block_size - 1:
-                    col += 1
-                    a = 1
-                else:  # 부딪히지 않았을 때
+            if flag:
+                if row in right and col == 7:  # block의 오른쪽 면에서 아래쪽으로 이동
                     row += 1
-                    col -= 1
-
-            else:
-                # 오른쪽 벽에 부딪혔을 때
-                if col == block_size - 1:
-                    row += 1
-                    a = 0
-                else:  # 부딪히지 않았을 때
+                    flag = False
+                else:
                     row -= 1
                     col += 1
-
+            else:
+                if col in down and row == 7:  # block의 아래쪽 면에서 오른쪽으로 이동
+                    col += 1
+                    flag = True
+                else:
+                    row += 1
+                    col -= 1
         if row == block_size - 1:
             half = False
 
+    length = len(zigzag)
     if encoding:
-        for idx, val in reversed(list(enumerate(zigzag))):
-            if val == 0:
-                del zigzag[idx]
-            else:
+        # 뒤쪽 0 제거
+
+        for i in range(length):
+            for j in range(i + 1, length, 1):
+                if i == 0:
+                    if i == j:
+                        eob = True
+                    else:
+                        eob = False
+                        break
+            if eob:
+                del zigzag[i:zigzag.len()]
                 zigzag.append('EOB')
-                break
+        return zigzag
+    else:  # decoding
+        return block_recover
 
-            return zigzag
 
-        else:  # decoding
-            return block_recover
-
-def DCT_inv(block, n = 8):
+def DCT_inv(block, n=8):
     ###################################################
     # TODO                                            #
     # DCT_inv 완성                                     #
     # DCT_inv 는 DCT와 다름.                            #
     ###################################################
-
     y, x = block.shape
     dst = np.zeros((y, x))
     v, u = dst.shape
@@ -174,20 +195,20 @@ def DCT_inv(block, n = 8):
             for v_ in range(v):
                 for u_ in range(u):
                     tmp += block[v_, u_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
-                           np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n)) * C(u_, n=n) * C(v_, n=n)
+                           np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n)) * C_matrix(u_, n=n) * C_matrix(v_, n=n)
             dst[y_, x_] = tmp
 
     return np.round(dst)
 
-def block2img(blocks, src_shape, n = 8):
+
+def block2img(blocks, src_shape, n=8):
     ###################################################
     # TODO                                            #
     # block2img 완성                                   #
     # 복구한 block들을 image로 만들기                     #
     ###################################################
-
     (h, w) = src_shape
-    # print('blocks', blocks, 'blocks shape', blocks.shape)
+
     if h % n != 0:
         h_pad = n - h % n
     else:
@@ -197,19 +218,20 @@ def block2img(blocks, src_shape, n = 8):
         w_pad = n - w % n
     else:
         w_pad = 0
-    dst = np.zeros((h + h_pad, w + w_pad), dtype=np.uint8)
-    idx = 0
 
-    print(blocks.shape)
-    print(dst.shape)
-    for row in range(int(h / 8)):
-        for col in range(int(w / 8)):
-            dst[8 * row:8 * (row + 1), 8 * col:8 * (col + 1)] = blocks[idx]
+    dst = np.zeros((h + h_pad, w + w_pad), dtype=np.uint8)
+
+    idx = 0
+    for row in range(int(h/8)):
+        for col in range(int(w/8)):
+            dst[row*8:(row+1)*8, col*8:(col+1)*8] = blocks[idx]
             idx += 1
 
+    dst = dst[0:h, 0:w]
     return dst
 
-def Encoding(src, n=8,scale_factor=1):
+
+def Encoding(src, n=8, scale_factor=10):
     #################################################################################################
     # TODO                                                                                          #
     # Encoding 완성                                                                                  #
@@ -218,31 +240,29 @@ def Encoding(src, n=8,scale_factor=1):
     print('<start Encoding>')
     # img -> blocks
     blocks = img2block(src, n=n)
-    print("block = \n",src[150:158,89:97])
+    print("block = \n", src[150:158, 89:97])
 
-
-    #subtract 128
+    # subtract 128
     blocks -= 128
-    b = np.double(src[150:158,89:97])-128
-    print("b = \n",b)
+    b = np.double(src[150:158, 89:97]) - 128
+    print("b = \n", b)
 
-    #DCT
+    # DCT
     blocks_dct = []
     for block in blocks:
         blocks_dct.append(DCT(block, n=n))
     blocks_dct = np.array(blocks_dct)
 
     # print DCT results
-    bd = DCT(b,n=8)
-    print("bd = \n",bd)
+    bd = DCT(b, n=8)
+    print("bd = \n", bd)
 
-
-    #Quantization + thresholding
+    # Quantization + thresholding
     Q = Quantization_Luminance(scale_factor)
     QnT = np.round(blocks_dct / Q)
-    #print Quantization results
-    bq = bd  / Q
-    print("bq = \n",bq)
+    # print Quantization results
+    bq = bd / Q
+    print("bq = \n", bq)
 
     # zigzag scanning
     zz = []
@@ -251,7 +271,8 @@ def Encoding(src, n=8,scale_factor=1):
 
     return zz, src.shape, bq
 
-def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
+
+def Decoding(zigzag, src_shape, bq, n=8, scale_factor=10):
     #################################################################################################
     # TODO                                                                                          #
     # Decoding 완성                                                                                  #
@@ -265,13 +286,12 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
         blocks.append(my_zigzag_scanning(zigzag[i], mode='decoding', block_size=n))
     blocks = np.array(blocks)
 
-
     # Denormalizing
     Q = Quantization_Luminance(scale_factor=scale_factor)
     blocks = blocks * Q
     # print results Block * Q
     bq2 = bq * Q
-    print("bq2 = \n",bq2)
+    print("bq2 = \n", bq2)
 
     # inverse DCT
     blocks_idct = []
@@ -279,16 +299,16 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
         blocks_idct.append(DCT_inv(block, n=n))
     blocks_idct = np.array(blocks_idct)
 
-    #print IDCT results
-    bd2 = DCT_inv(bq2,n=8)
-    print("bd2 = \n",bd2)
+    # print IDCT results
+    bd2 = DCT_inv(bq2, n=8)
+    print("bd2 = \n", bd2)
 
     # add 128
     blocks_idct += 128
 
     # print block value
     b2 = np.round(bd2 + 128)
-    print("b2 = \n",b2)
+    print("b2 = \n", b2)
 
     # block -> img
     dst = block2img(blocks_idct, src_shape=src_shape, n=n)
@@ -296,20 +316,21 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
     return dst, b2
 
 
-
 def main():
-    scale_factor = 1
+    scale_factor = 10
     start = time.time()
-    src = cv2.imread('caribou.tif', cv2.IMREAD_GRAYSCALE)
+    # src = cv2.imread('../imgs/Lenna.png', cv2.IMREAD_GRAYSCALE)
+    src = cv2.imread('./caribou.tif', cv2.IMREAD_GRAYSCALE)
 
-    comp, src_shape,bq = Encoding(src, n=8,scale_factor=scale_factor)
+    comp, src_shape, bq = Encoding(src, n=8, scale_factor=scale_factor)
     np.save('comp.npy', comp)
     np.save('src_shape.npy', src_shape)
     # print(comp)
     comp = np.load('comp.npy', allow_pickle=True)
     src_shape = np.load('src_shape.npy')
-    recover_img, b2 = Decoding(comp, src_shape, bq,n=8,scale_factor=scale_factor)
-    print("scale_factor : ",scale_factor,"differences between original and reconstructed = \n",src[150:158,89:97]-b2)
+    recover_img, b2 = Decoding(comp, src_shape, bq, n=8, scale_factor=scale_factor)
+    print("scale_factor : ", scale_factor, "differences between original and reconstructed = \n",
+          src[150:158, 89:97] - b2)
     # print(recover_img)
     total_time = time.time() - start
     #
@@ -317,7 +338,7 @@ def main():
     if total_time > 12:
         print('감점 예정입니다.')
     print(recover_img.shape)
-    cv2.imshow('recover img', recover_img)
+    cv2.imshow('20181584 kyw', recover_img)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
--- hw_11/test3.py
+++ hw_11/test3.py
@@ -15,11 +15,6 @@ def Quantization_Luminance(scale_factor):
     return luminance * scale_factor
 
 def img2block(src, n=8):
-    ######################################
-    # TODO                               #
-    # img2block 완성                      #
-    # img를 block으로 변환하기              #
-    ######################################
     (h, w) = src.shape
 
     if h % n != 0:
@@ -40,89 +35,102 @@ def img2block(src, n=8):
         for col in range(0, w+w_pad, n):
             block = dst[row:row+n, col:col+n]
             blocks.append(block)
-
+    ######################################
+    # TODO                               #
+    # img2block 완성                      #
+    # img를 block으로 변환하기              #
+    ######################################
     return np.array(blocks)
 
-# --- 추가
-def C(w, n=8):
+def C(w, n = 8):
     if w == 0:
-        return (1/n) ** 0.5
+        return (1 / n) ** 0.5
     else:
-        return (2/n) ** 0.5
-
+        return (2 / n) ** 0.5
 
 
 def DCT(block, n=8):
     ######################################
     # TODO                               #
     # DCT 완성                            #
-    # 4중 for문으로 구현 시 감점 예정          #
     ######################################
-    v, u = block.shape
+
     y, x = block.shape
-    dst = np.zeros(block.shape)
+    dst = np.zeros((y, x))
+    v, u = dst.shape
     for v_ in range(v):
         for u_ in range(u):
             tmp = 0
             for y_ in range(y):
                 for x_ in range(x):
-                    tmp ++ block[y_, x_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
+                    tmp += block[y_, x_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
                            np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n))
+
             dst[v_, u_] = C(u_, n=n) * C(v_, n=n) * tmp
 
     return np.round(dst)
 
-def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
+
+
+
+
+def my_zigzag_scanning(block, mode='encoding', block_size=8):
     ######################################
     # TODO                               #
     # my_zigzag_scanning 완성             #
     ######################################
     row = 0
     col = 0
-    val = 1
     idx = 0
-    half = False
-    half_count = 0
+    half = True
     zigzag = []
     block_recover = np.zeros((block_size, block_size))
-    change = False
     encoding = True
-
     a = 1
+    # print('zigzag before')
+    # print(block)
     if mode == 'decoding':
         encoding = False
 
     while True:
+        # print('row', row, ' col', col, ' val', block[row][col], ' a', a, ' half', half, ' half_count', half_count)
         if encoding:
             zigzag.append(block[row][col])
         else:
-            if idx >= len(block) or block[idx] == 'E0B':
+            if idx >= len(block) or block[idx] == 'EOB': #decoding 종료조건
                 break
             block_recover[row][col] = block[idx]
             idx += 1
 
-        # 종료 조건
-        if row == block_size - 1 and col == block_size - 1:
+        #종료 조건
+        if row == block_size-1 and col == block_size-1:
             break
 
+        # 절반을 넘지 않았을 때
         if half:
+            # 아래쪽 대각선 방향일때
             if a == 0:
+                # 왼쪽 벽에 부딪혔을 때
                 if col == 0:
                     row += 1
                     a = 1
-                else:
+                else: # 부딪히지 않았을 때
                     row += 1
                     col -= 1
 
+            # 위쪽 대각선 방향일 때
             else:
+                # 위쪽 벽에 부딪혔을 때
                 if row == 0:
                     col += 1
                     a = 0
-                else:
+                else: # 부딪히지 않았을 때
                     row -= 1
                     col += 1
 
-        else:
+        # 절반을 넘겼을 때
+        else: #half == False
+            # 아래쪽 대각선 방향일때
             if a == 0:
                 # 아랫쪽 벽에 부딪혔을 때
                 if row == block_size - 1:
@@ -132,6 +140,7 @@ def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
                     row += 1
                     col -= 1
 
+            # 위쪽 대각선 방향일 때
             else:
                 # 오른쪽 벽에 부딪혔을 때
                 if col == block_size - 1:
@@ -145,17 +154,22 @@ def my_zigzag_scanning(block, mode = 'encoding', block_size=8):
             half = False
 
     if encoding:
+        #뒤쪽 0 제거
         for idx, val in reversed(list(enumerate(zigzag))):
             if val == 0:
                 del zigzag[idx]
             else:
                 zigzag.append('EOB')
                 break
+        # print('zigzag after')
+        # print(zigzag)
+        # print(type(zigzag))
+        return zigzag
 
-            return zigzag
-
-        else:  # decoding
-            return block_recover
+    else: #decoding
+        # print('zigzag decoding')
+        # print(block_recover)
+        return block_recover
 
 def DCT_inv(block, n = 8):
     ###################################################
@@ -174,7 +188,7 @@ def DCT_inv(block, n = 8):
             for v_ in range(v):
                 for u_ in range(u):
                     tmp += block[v_, u_] * np.cos(((2 * x_ + 1) * u_ * np.pi) / (2 * n)) * \
-                           np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n)) * C(u_, n=n) * C(v_, n=n)
+                           np.cos(((2 * y_ + 1) * v_ * np.pi) / (2 * n))* C(u_, n=n) * C(v_, n=n)
             dst[y_, x_] = tmp
 
     return np.round(dst)
@@ -185,26 +199,26 @@ def block2img(blocks, src_shape, n = 8):
     # block2img 완성                                   #
     # 복구한 block들을 image로 만들기                     #
     ###################################################
-
     (h, w) = src_shape
     # print('blocks', blocks, 'blocks shape', blocks.shape)
     if h % n != 0:
-        h_pad = n - h % n
+        h_pad = n - h%n
     else:
         h_pad = 0
 
     if w % n != 0:
-        w_pad = n - w % n
+        w_pad = n - w%n
     else:
         w_pad = 0
-    dst = np.zeros((h + h_pad, w + w_pad), dtype=np.uint8)
+    dst = np.zeros((h+h_pad, w+w_pad), dtype=np.uint8)
     idx = 0
 
     print(blocks.shape)
     print(dst.shape)
-    for row in range(int(h / 8)):
-        for col in range(int(w / 8)):
-            dst[8 * row:8 * (row + 1), 8 * col:8 * (col + 1)] = blocks[idx]
+    for row in range(int(h/8)):
+        for col in range(int(w/8)):
+
+            dst[8*row:8*(row+1), 8*col:8*(col+1)] = blocks[idx]
             idx += 1
 
     return dst
@@ -224,7 +238,7 @@ def Encoding(src, n=8,scale_factor=1):
     #subtract 128
     blocks -= 128
     b = np.double(src[150:158,89:97])-128
-    print("b = \n",b)
+    print("subtract 128 b = \n",b)
 
     #DCT
     blocks_dct = []
@@ -234,7 +248,7 @@ def Encoding(src, n=8,scale_factor=1):
 
     # print DCT results
     bd = DCT(b,n=8)
-    print("bd = \n",bd)
+    print("DCT bd = \n",bd)
 
 
     #Quantization + thresholding
@@ -242,7 +256,7 @@ def Encoding(src, n=8,scale_factor=1):
     QnT = np.round(blocks_dct / Q)
     #print Quantization results
     bq = bd  / Q
-    print("bq = \n",bq)
+    print("Quantization + thresholding bq = \n",bq)
 
     # zigzag scanning
     zz = []
@@ -265,23 +279,26 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
         blocks.append(my_zigzag_scanning(zigzag[i], mode='decoding', block_size=n))
     blocks = np.array(blocks)
 
+    # print('zigzag', blocks)
+
 
     # Denormalizing
     Q = Quantization_Luminance(scale_factor=scale_factor)
     blocks = blocks * Q
     # print results Block * Q
     bq2 = bq * Q
-    print("bq2 = \n",bq2)
+    print("Denormalizing bq2 = \n",bq2)
 
     # inverse DCT
     blocks_idct = []
     for block in blocks:
         blocks_idct.append(DCT_inv(block, n=n))
+
     blocks_idct = np.array(blocks_idct)
 
     #print IDCT results
     bd2 = DCT_inv(bq2,n=8)
-    print("bd2 = \n",bd2)
+    print("IDCT results bd2 = \n",bd2)
 
     # add 128
     blocks_idct += 128
@@ -290,6 +307,9 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
     b2 = np.round(bd2 + 128)
     print("b2 = \n",b2)
 
+    # print('block2img before blocks_idct')
+    # print(blocks_idct.shape)
+    # print(blocks_idct)
     # block -> img
     dst = block2img(blocks_idct, src_shape=src_shape, n=n)
 
@@ -300,15 +320,16 @@ def Decoding(zigzag, src_shape,bq, n=8,scale_factor=1):
 def main():
     scale_factor = 1
     start = time.time()
-    src = cv2.imread('caribou.tif', cv2.IMREAD_GRAYSCALE)
+    # src = cv2.imread('../imgs/Lenna.png', cv2.IMREAD_GRAYSCALE)
+    src = cv2.imread('./caribou.tif', cv2.IMREAD_GRAYSCALE)
 
-    comp, src_shape,bq = Encoding(src, n=8,scale_factor=scale_factor)
+    comp, src_shape, bq = Encoding(src, n=8,scale_factor=scale_factor)
     np.save('comp.npy', comp)
     np.save('src_shape.npy', src_shape)
     # print(comp)
     comp = np.load('comp.npy', allow_pickle=True)
     src_shape = np.load('src_shape.npy')
-    recover_img, b2 = Decoding(comp, src_shape, bq,n=8,scale_factor=scale_factor)
+    recover_img, b2 = Decoding(comp, src_shape, bq, n=8,scale_factor=scale_factor)
     print("scale_factor : ",scale_factor,"differences between original and reconstructed = \n",src[150:158,89:97]-b2)
     # print(recover_img)
     total_time = time.time() - start
@@ -317,7 +338,8 @@ def main():
     if total_time > 12:
         print('감점 예정입니다.')
     print(recover_img.shape)
-    cv2.imshow('recover img', recover_img)
+    cv2.imwrite('./result/20181602 sf 1.jpeg',recover_img)
+    cv2.imshow('20181602 Choi Chang Su', recover_img)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
